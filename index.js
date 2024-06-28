783af62154dd29ab2331782e1bd8242dd5284abd
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2024-06-29 01:58:27 +0530

    My Portfolio uploaded

---
+++ index.js
@@ -0,0 +1,36 @@
+// console.log("Script runnig...");
+// document.querySelector('.cross').Style.display = 'none';
+// document.querySelector('.hamburger').addEventListener("click",()=>{
+//     document.querySelector('.sidebar').classList.toggle('sidebarGo');
+//     if(document.querySelector('.sidebar').classList.toggle('sidebarGo')){
+//         document.querySelector('.ham').Style.display='inline';
+//         document.querySelector('.cross').Style.display='none';
+//     }else{
+//         document.querySelector('.ham').Style.display='none';
+//         setTimeout(()=>{
+//             document.querySelector('.cross').Style.display='inline';
+//         },300)
+
+//     }
+
+// })
+<div class="main">
+            <div class="hamburger">
+                <i class="fa-solid fa-bars ham"></i>
+                <i class="fa-solid fa-xmark cross"></i>
+            </div>
+                <div class="intro">
+                    <h2>About me</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolorem asperiores explicabo assumenda ex, ipsam autem perspiciatis exercitationem, velit quae facere doloremque porro. Magni possimus, debitis, nemo quod adipisci reprehenderit eum iste modi id culpa quas in!</p>
+                
+                <h2>My Skills</h2>
+                <div class="skillcontainer">
+                    <div class="skillitem">HTML :<div class="skill t5"></div></div>
+                    <div class="skillitem">CSS :<div class="skill"></div></div>
+                    <div class="skillitem">JavaScript :<div class="skill"></div></div>
+                    <div class="skillitem">Php :<div class="skill t5"></div></div>
+                    <div class="skillitem">Python :<div class="skill"></div></div>
+                </div>
+            
+                </div>
+</div>
