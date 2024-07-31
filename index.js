ab0d5fe903803f7ff4e652cb6c6beb955ebf67bd
mahammedafzal786 <afzalkhaliddiwan2003@gmail.com>
2024-07-31 11:30:21 +0530

    formate code

--- index.js
+++ index.js
@@ -15,22 +15,36 @@
 
 // })
 <div class="main">
-            <div class="hamburger">
-                <i class="fa-solid fa-bars ham"></i>
-                <i class="fa-solid fa-xmark cross"></i>
-            </div>
-                <div class="intro">
-                    <h2>About me</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolorem asperiores explicabo assumenda ex, ipsam autem perspiciatis exercitationem, velit quae facere doloremque porro. Magni possimus, debitis, nemo quod adipisci reprehenderit eum iste modi id culpa quas in!</p>
-                
-                <h2>My Skills</h2>
-                <div class="skillcontainer">
-                    <div class="skillitem">HTML :<div class="skill t5"></div></div>
-                    <div class="skillitem">CSS :<div class="skill"></div></div>
-                    <div class="skillitem">JavaScript :<div class="skill"></div></div>
-                    <div class="skillitem">Php :<div class="skill t5"></div></div>
-                    <div class="skillitem">Python :<div class="skill"></div></div>
-                </div>
-            
-                </div>
-</div>
+  <div class="hamburger">
+    <i class="fa-solid fa-bars ham" />
+    <i class="fa-solid fa-xmark cross" />
+  </div>
+  <div class="intro">
+    <h2>About me</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolorem
+      asperiores explicabo assumenda ex, ipsam autem perspiciatis
+      exercitationem, velit quae facere doloremque porro. Magni possimus,
+      debitis, nemo quod adipisci reprehenderit eum iste modi id culpa quas in!
+    </p>
+
+    <h2>My Skills</h2>
+    <div class="skillcontainer">
+      <div class="skillitem">
+        HTML :<div class="skill t5" />
+      </div>
+      <div class="skillitem">
+        CSS :<div class="skill" />
+      </div>
+      <div class="skillitem">
+        JavaScript :<div class="skill" />
+      </div>
+      <div class="skillitem">
+        Php :<div class="skill t5" />
+      </div>
+      <div class="skillitem">
+        Python :<div class="skill" />
+      </div>
+    </div>
+  </div>
+</div>;
